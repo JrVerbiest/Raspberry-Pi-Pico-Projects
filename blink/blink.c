825ed13f8ab5501586dba509220ab3c8db20818e
J. R. Verbiest <39417102+JrVerbiest@users.noreply.github.com>
2023-03-29 18:11:52 +0200

    update blink

--- blink/blink.c
+++ blink/blink.c
@@ -1,23 +1,26 @@
 /**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
+ * Blink - Hello world.
  */
 
+#include <stdio.h>
 #include "pico/stdlib.h"
 
 int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(200);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(200);
-    }
-#endif
+    stdio_init_all();
+
+    #ifndef PICO_DEFAULT_LED_PIN
+    #warning blink example requires a board with a regular LED
+    #else
+        const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+        gpio_init(LED_PIN);
+        gpio_set_dir(LED_PIN, GPIO_OUT);
+
+        while (true) {
+            gpio_put(LED_PIN, 1);          
+            printf('Hello, world! \n');
+            sleep_ms(1000);
+            gpio_put(LED_PIN, 0);
+            sleep_ms(1000);
+        }
+    #endif
 }
