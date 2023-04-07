3047459228aceca694e8b30014fc26c7af69fe63
J. R. Verbiest <39417102+JrVerbiest@users.noreply.github.com>
2023-04-07 14:55:02 +0200

    update blink

--- blink/blink.c
+++ blink/blink.c
@@ -6,21 +6,23 @@
 #include "pico/stdlib.h"
 
 int main() {
+
+    const uint led_pin = 25;
+
+    // Initialize LED pin
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    // Initialize chosen serial port
     stdio_init_all();
 
-    #ifndef PICO_DEFAULT_LED_PIN
-    #warning blink example requires a board with a regular LED
-    #else
-        const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-        gpio_init(LED_PIN);
-        gpio_set_dir(LED_PIN, GPIO_OUT);
 
-        while (true) {
-            gpio_put(LED_PIN, 1);          
-            printf('Hello, world! \n');
-            sleep_ms(1000);
-            gpio_put(LED_PIN, 0);
-            sleep_ms(1000);
-        }
-    #endif
+    while(true){
+        // Blink LED
+        printf("Blinking!\r\n");
+        gpio_put(led_pin, true);
+        sleep_ms(1000);
+        gpio_put(led_pin, false);
+        sleep_ms(1000);
+    }
 }
